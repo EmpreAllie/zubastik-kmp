d0a72b7d7c2fb22b53920eef2bd14a7850705b1a
EmpreAllie <kkoltsov2004@gmail.com>
2025-10-09 11:18:01 +0300

    ZUB-1: Рефакторинг

---
+++ androidApp/features/root/src/main/kotlin/com/zubastikmobile/root/RootActivity.kt
@@ -0,0 +1,29 @@
+package com.zubastikmobile.root
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.core.data.utils.globalApplicationContext
+import com.features.root.ui.RootApp
+import com.root.presentation.RootViewModel
+import org.koin.androidx.compose.KoinAndroidContext
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
+
+class RootActivity : ComponentActivity() {
+    @OptIn(KoinExperimentalAPI::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        globalApplicationContext = applicationContext
+
+        setContent {
+            KoinAndroidContext {
+                val viewModel: RootViewModel = koinViewModel()
+                RootApp(viewModel)
+            }
+        }
+    }
+}
---
+++ androidApp/features/root/src/main/kotlin/com/zubastikmobile/root/RootScreenApplication.kt
@@ -0,0 +1,18 @@
+package com.zubastikmobile.root
+
+import android.app.Application
+import com.root.di.startKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
+
+class RootScreenApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@RootScreenApplication)
+            androidLogger(level = Level.DEBUG)
+        }
+    }
+}
